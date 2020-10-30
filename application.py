dff5f22826dfe1e9282441d0164e994883500467
jaredb <jrdbuch@yahoo.com>
2020-10-29 21:31:10 -0700

    added docstring checker

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
+"""Habit Tracker Web Server entry point."""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+   """Hello world"""
    return 'Hello World'
 
 if __name__ == '__main__':
