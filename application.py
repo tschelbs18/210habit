0125f0848a5c1128336176905866cf28dafeca1c
tschelbs18 <ted.h.schelble@gmail.com>
2020-10-30 12:37:30 -0400

    Fixing indentation spacing lol

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    ''' Adding Docstring to satisfy github actions '''
+   ''' Adding Docstring to satisfy github actions '''
    return 'Hello, World!!'
 
 if __name__ == '__main__':
