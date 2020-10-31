4f4bfec60b9692674f6ff74480475f797f0dfe25
Daniel Koohmarey <daniel.koohmarey@berkeley.edu>
2020-10-31 13:11:08 -0700

    Update initial files
    Add flask & pytest to requirements.txt, and team page,  update sample unit test, update readme with getting started

--- application.py
+++ application.py
@@ -7,5 +7,13 @@ def hello_world():
    """Adding Docstring to satisfy github actions."""
    return 'Hello, World!!'
 
+@app.route('/team/<member>')  # i.e /team/daniel
+def team_page(member):
+    """ Route for team members, each member is associated with their own route """
+    if member == "daniel":
+        return "Daniel's page!"
+    else:
+        return "Unknown team member: {}".format(member)
+
 if __name__ == '__main__':
    app.run()
