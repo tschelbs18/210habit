a2cbca26d4f4b1771cc9994aee95ae6acbcf8e92
jaredb <jrdbuch@yahoo.com>
2020-12-02 20:55:13 -0800

    autopep8

--- src/habit_server/__init__.py
+++ src/habit_server/__init__.py
@@ -1,14 +1,15 @@
 """habit_server package root."""
 import os
 import uuid
-from flask import Flask, session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
 app.config['SECRET_KEY'] = uuid.uuid4().hex
 db = SQLAlchemy(app)
 
