e1a5bcb98260e7964a67d6f69f0d8b8d28cf1efd
tschelbs18 <ted.h.schelble@gmail.com>
2020-12-07 13:11:39 -0500

    remove url_for reference in imports as it is unused

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 from datetime import date
 import os
 import uuid
-from flask import render_template, request, redirect, Flask, url_for, flash
+from flask import render_template, request, redirect, Flask, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_login import LoginManager
 from src.db_models import User, UserActivity, UserHabit, db
